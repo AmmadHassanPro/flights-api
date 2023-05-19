b7a33eaec111e6d4d368175282cca3c0e6805c90
Syed Ammad Hassan <ammad13k@gmail.com>
2023-05-19 15:51:09 -0500

    Added @Service annotaion

--- src/main/java/com/atombooking/flightsapi/service/CityAirportService.java
+++ src/main/java/com/atombooking/flightsapi/service/CityAirportService.java
@@ -1,5 +1,8 @@
 package com.atombooking.flightsapi.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface CityAirportService {
 
 	String getCity(String keyword);
--- src/main/java/com/atombooking/flightsapi/service/FlightOffersService.java
+++ src/main/java/com/atombooking/flightsapi/service/FlightOffersService.java
@@ -1,8 +1,10 @@
 package com.atombooking.flightsapi.service;
 
+import org.springframework.stereotype.Service;
+
 import com.atombooking.flightsapi.request.flightoffers.FlightOfferRequest;
 import com.atombooking.flightsapi.response.flightofffers.FlightOffersResponse;
-
+@Service
 public interface FlightOffersService {
 
 	FlightOffersResponse getFlightOffers(FlightOfferRequest req) ;
