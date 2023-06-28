bac6b353264fed97669ca11eb12802791d5e08b2
Syed Ammad Hassan <ammad13k@gmail.com>
2023-06-27 21:47:23 -0500

    Refactord getCityAndAirports Endpoint
    Added a response entiry , returned proper statuses

--- src/main/java/com/atombooking/flightsapi/controller/MainController.java
+++ src/main/java/com/atombooking/flightsapi/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.atombooking.flightsapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,11 @@ public class MainController {
 	CityAirportService service;
 	
 	@GetMapping("getCityAndAirport/{keyword}")
-	public LocationApiDto getCityAndAirports(@PathVariable String keyword) {
-		return service.getCityAndAirport(keyword);
+	public ResponseEntity<LocationApiDto> getCityAndAirports(@PathVariable String keyword) {
+		LocationApiDto resp =  service.getCityAndAirport(keyword);
+		if(resp.getData().size() == 0) {
+			return new ResponseEntity<>(new LocationApiDto(), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(resp, HttpStatus.OK);
 	}
 }
