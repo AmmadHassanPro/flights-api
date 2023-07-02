ffce73a32a1d4d22c60f1b7f1ce78b41249674ca
Syed Ammad Hassan <ammad13k@gmail.com>
2023-07-02 17:04:09 -0500

    changed test to junit5

--- src/test/java/com/atombooking/flightsapi/converters/LocationApiDtoConverterTest.java
+++ src/test/java/com/atombooking/flightsapi/converters/LocationApiDtoConverterTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.atombooking.flightsapi.response.locationapi.Address;
 import com.atombooking.flightsapi.response.locationapi.Datum;
