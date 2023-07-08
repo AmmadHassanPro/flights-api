f21aa3d3301e64af24b94a52407bb31e054360dc
Syed Ammad Hassan <ammad13k@gmail.com>
2023-07-07 19:53:03 -0500

    Changed FlightOffersService
    Changed from deprecated Date to LocalDate

--- src/main/java/com/atombooking/flightsapi/service/FlightOffersService.java
+++ src/main/java/com/atombooking/flightsapi/service/FlightOffersService.java
@@ -1,6 +1,6 @@
 package com.atombooking.flightsapi.service;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import org.springframework.stereotype.Service;
 
@@ -8,5 +8,5 @@ import com.atombooking.flightsapi.response.flightofffers.FlightOffersResponse;
 @Service
 public interface FlightOffersService {
 
-	FlightOffersResponse getFlightOffers(String Orig, String Dest , Date Depart, Date Return, Integer numberOfAdults) ;
+	FlightOffersResponse getFlightOffers(String Orig, String Dest , LocalDate Depart, LocalDate Return, Integer numberOfAdults) ;
 }
