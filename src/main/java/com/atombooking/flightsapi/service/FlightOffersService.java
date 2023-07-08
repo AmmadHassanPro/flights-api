1ba260c01147f7b9b0e0dcc28d8a439c12f445d2
Syed Ammad Hassan <ammad13k@gmail.com>
2023-07-07 19:18:42 -0500

    Changed params as needed

--- src/main/java/com/atombooking/flightsapi/service/FlightOffersService.java
+++ src/main/java/com/atombooking/flightsapi/service/FlightOffersService.java
@@ -1,11 +1,12 @@
 package com.atombooking.flightsapi.service;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
-import com.atombooking.flightsapi.request.flightoffers.FlightOfferRequest;
 import com.atombooking.flightsapi.response.flightofffers.FlightOffersResponse;
 @Service
 public interface FlightOffersService {
 
-	FlightOffersResponse getFlightOffers(FlightOfferRequest req) ;
+	FlightOffersResponse getFlightOffers(String Orig, String Dest , Date Depart, Date Return, Integer numberOfAdults) ;
 }
