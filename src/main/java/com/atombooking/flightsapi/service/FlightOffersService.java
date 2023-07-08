7d20bacd6dba9522b6f1522600cc34200b7babd5
Syed Ammad Hassan <ammad13k@gmail.com>
2023-07-08 16:53:12 -0500

    Refactored flightOffers Service

--- src/main/java/com/atombooking/flightsapi/service/FlightOffersService.java
+++ src/main/java/com/atombooking/flightsapi/service/FlightOffersService.java
@@ -1,6 +1,7 @@
 package com.atombooking.flightsapi.service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -8,5 +9,5 @@ import com.atombooking.flightsapi.response.flightofffers.FlightOffersResponse;
 @Service
 public interface FlightOffersService {
 
-	FlightOffersResponse getFlightOffers(String Orig, String Dest , LocalDate Depart, LocalDate Return, Integer numberOfAdults) ;
+	FlightOffersResponse getFlightOffers(String Orig, String Dest , LocalDate Depart, Optional<LocalDate> Return, Integer numberOfAdults , boolean nonStop) ;
 }
