16ce0c271acd6f812c683fbb7fe7855d4043f18d
Syed Ammad Hassan <ammad13k@gmail.com>
2023-07-14 19:21:59 -0500

    Added a new endpoint to controller
    Added a new endpoint to controller, to fetch flight offers based on
    different criteria

--- src/main/java/com/atombooking/flightsapi/controller/MainController.java
+++ src/main/java/com/atombooking/flightsapi/controller/MainController.java
@@ -1,12 +1,17 @@
 package com.atombooking.flightsapi.controller;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.atombooking.flightsapi.response.flightofffers.FlightOffersResponse;
 import com.atombooking.flightsapi.response.locationapi.LocationApiDto;
 import com.atombooking.flightsapi.service.CityAirportService;
 import com.atombooking.flightsapi.service.FlightOffersService;
@@ -30,5 +35,21 @@ public class MainController {
 			return new ResponseEntity<>(new LocationApiDto(), HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<>(resp, HttpStatus.OK);
+	}
+	
+	@GetMapping("get-offers")
+	public ResponseEntity<FlightOffersResponse> getFlightOffers(@RequestParam String originLocationCode , @RequestParam String destinationLocationCode , 
+			@RequestParam String departureDate , @RequestParam Optional<String> returnDate, @RequestParam Integer adults, @RequestParam Boolean nonStop){
+		
+		LocalDate dep = LocalDate.parse(departureDate);
+		Optional<LocalDate> ret =  (returnDate.isEmpty()) ? Optional.empty() : Optional.of(LocalDate.parse(returnDate.get()));
+		
+		FlightOffersResponse resp = fOService.getFlightOffers(originLocationCode, destinationLocationCode, dep, ret, adults, nonStop);
+		
+		if(resp== null || resp.getData()== null || resp.getData().size()==0) {
+			return new ResponseEntity<>(new FlightOffersResponse(), HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(resp, HttpStatus.OK);
+		
 	}
 }
