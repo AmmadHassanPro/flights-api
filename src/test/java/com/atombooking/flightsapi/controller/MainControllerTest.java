6abff1c9a1d5f05f1785b0c32c9319136ae4e72f
Syed Ammad Hassan <ammad13k@gmail.com>
2023-07-14 19:52:19 -0500

    Added test for new endpoint
    Created mockMVC test for main Contollers new endpoint 'get-offers'

--- src/test/java/com/atombooking/flightsapi/controller/MainControllerTest.java
+++ src/test/java/com/atombooking/flightsapi/controller/MainControllerTest.java
@@ -7,22 +7,31 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.MultiValueMap;
 
 import com.atombooking.flightsapi.converters.LocationApiDtoConverter;
+import com.atombooking.flightsapi.response.flightofffers.Data;
+import com.atombooking.flightsapi.response.flightofffers.FlightOffersResponse;
 import com.atombooking.flightsapi.response.locationapi.Address;
 import com.atombooking.flightsapi.response.locationapi.Datum;
 import com.atombooking.flightsapi.response.locationapi.LocationAPIResponse;
 import com.atombooking.flightsapi.response.locationapi.LocationApiDto;
 import com.atombooking.flightsapi.service.CityAirportService;
+import com.atombooking.flightsapi.service.FlightOffersService;
 
 
 @WebMvcTest(MainController.class)
@@ -34,13 +43,16 @@ public class MainControllerTest {
 	private MockMvc mockMvc;
 	
 	@MockBean
-	private CityAirportService service;
+	private CityAirportService cAService;
+	
+	@MockBean
+	private FlightOffersService fOService;
 	
 	
 	@Test
-	public void shouldReturnValidRespwithValidIn() throws Exception {
+	public void testgetCityAndAirports() throws Exception {
 		
-		when(service.getCityAndAirport(eq("Chicago"))).thenReturn(getTestResp());
+		when(cAService.getCityAndAirport(eq("Chicago"))).thenReturn(getTestResp());
 		
 		this.mockMvc.perform(get("/v1/flights-api/get-city-and-airport/Chicago")).andExpect(status().isOk())
 		.andExpect(content().string(containsString("O HARE INTERNATIONAL")));
@@ -48,12 +60,37 @@ public class MainControllerTest {
 	}
 	
 	@Test
-	public void shouldReturn404() throws Exception {
-		when(service.getCityAndAirport(eq("nonExistentCity"))).thenReturn(new LocationApiDto());
+	public void shouldReturn404ForCityAndAirports() throws Exception {
+		when(cAService.getCityAndAirport(eq("nonExistentCity"))).thenReturn(new LocationApiDto());
 		
 		this.mockMvc.perform(get("/v1/flights-api/get-city-and-airport/nonExistentCity")).andExpect(status().isNotFound());
 		
 	}
+	
+	@Test
+	public void testgetFlightOffers() throws Exception {
+		
+		LocalDate dep = LocalDate.now().plusDays(10);
+		LocalDate ret = LocalDate.now().plusDays(20); 
+		String source = "SYD";
+		String dest = "BKK";
+		int numOfAdults = 5;
+		boolean nonStop = true;
+		
+		MultiValueMap<String,String> queryParams = new HttpHeaders();
+		queryParams.add("originLocationCode" , source);
+		queryParams.add("destinationLocationCode" , dest);
+		queryParams.add("departureDate" , dep.toString());
+		queryParams.add("returnDate" , ret.toString());
+		queryParams.add("adults" , String.valueOf(numOfAdults));
+		queryParams.add("nonStop" , String.valueOf(nonStop));
+		
+		when(fOService.getFlightOffers(source, dest, dep, Optional.of(ret), numOfAdults, nonStop)).thenReturn(getFlightOfferTestResp());
+		
+		this.mockMvc.perform(get("/v1/flights-api/get-offers").queryParams(queryParams)).andExpect(status().isOk());
+		
+	}
+	
 	
 	private LocationApiDto getTestResp() {
 		LocationAPIResponse testResp = new LocationAPIResponse();
@@ -87,5 +124,15 @@ public class MainControllerTest {
 		
 		return LocationApiDtoConverter.getInstance().convertToDto(testResp);
 	}
-
+	
+	// generates a dummy response with no actual values
+	private FlightOffersResponse getFlightOfferTestResp() {
+		
+		FlightOffersResponse rep = new FlightOffersResponse();
+		List<Data> dataList = new ArrayList<>();
+		dataList.add(new Data());
+		rep.setData(dataList);
+		
+		return rep;
+	}
 }
