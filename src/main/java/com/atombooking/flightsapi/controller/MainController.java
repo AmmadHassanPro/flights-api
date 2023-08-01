ee3b0cbb548caead72eeb25566720b1ef65fa709
Syed Ammad Hassan <ammad13k@gmail.com>
2023-07-31 21:02:57 -0500

    Added headers and logged uuids

--- src/main/java/com/atombooking/flightsapi/controller/MainController.java
+++ src/main/java/com/atombooking/flightsapi/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,28 +37,30 @@ public class MainController {
 	}
 	
 	@GetMapping(EndpointUrls.GET_CITY_AND_AIRPORT+"/{keyword}")
-	public ResponseEntity<LocationApiDto> getCityAndAirports(@PathVariable String keyword) {
-		logger.info("Endpoint url "+EndpointUrls.GET_CITY_AND_AIRPORT+"/"+keyword);
+	public ResponseEntity<LocationApiDto> getCityAndAirports(@PathVariable String keyword, @RequestHeader(name="consumer-name") String consumerName
+			,@RequestHeader(name="request-uuid") String requuestUUID) {
+		String headersLog= "\n Consumer Name: "+consumerName+" , Request UUID: "+requuestUUID+"\n";
+		logger.info("Endpoint url "+EndpointUrls.GET_CITY_AND_AIRPORT+"/"+keyword + headersLog);
 		LocationApiDto resp = null;
 		try {
 		resp =  cAService.getCityAndAirport(keyword);
 		}
 		catch(Exception ex) {
-			logger.info(ex.toString());
-			logger.info("returning: "+ HttpStatus.INTERNAL_SERVER_ERROR);
+			logger.info(headersLog + ", Exception: "+ ex.toString());
+			logger.info("returning: "+ HttpStatus.INTERNAL_SERVER_ERROR + headersLog) ;
 			return new ResponseEntity<>(new LocationApiDto(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		if( resp.getData() == null || resp.getData().size() == 0) {
-			logger.info("Empty Response");
-			logger.info("returning: "+ HttpStatus.NOT_FOUND);
+			logger.info("Empty Response" + headersLog);
+			logger.info("returning: "+ HttpStatus.NOT_FOUND + headersLog);
 			return new ResponseEntity<>(new LocationApiDto(), HttpStatus.NOT_FOUND);
 		}
-		logger.info("Returning : " + HttpStatus.OK);
+		logger.info("Returning : " + HttpStatus.OK +headersLog);
 		if(logger.isDebugEnabled()) {
 			try {
-				logger.debug("Resposne Returned : " + mapper.writeValueAsString(resp));
+				logger.debug("Resposne Returned : " + mapper.writeValueAsString(resp) + headersLog);
 			} catch (JsonProcessingException e) {
-				logger.info("Exception while writing value as String: " + e);
+				logger.info("Exception while writing value as String: " + e + headersLog);
 			}
 		}
 		return new ResponseEntity<>(resp, HttpStatus.OK);
@@ -65,10 +68,13 @@ public class MainController {
 	
 	@GetMapping(EndpointUrls.GET_OFFERS)
 	public ResponseEntity<FlightOffersResponse> getFlightOffers(@RequestParam String originLocationCode , @RequestParam String destinationLocationCode , 
-			@RequestParam String departureDate , @RequestParam Optional<String> returnDate, @RequestParam Integer adults, @RequestParam Boolean nonStop){
-		logger.info("Endpoint Url "+EndpointUrls.GET_OFFERS);
+			@RequestParam String departureDate , @RequestParam Optional<String> returnDate, @RequestParam Integer adults, @RequestParam Boolean nonStop,
+			@RequestHeader(name="consumer-name") String consumerName
+			,@RequestHeader(name="request-uuid") String requuestUUID){
+		String headersLog= "\n Consumer Name: "+consumerName+" , Request UUID: "+requuestUUID+"\n";
+		logger.info("Endpoint Url "+EndpointUrls.GET_OFFERS + headersLog);
 		logger.debug("originLocationCode:"+originLocationCode+", destinationLocationCode:"+destinationLocationCode+", "
-				+ "departureDate:"+departureDate+", returnDate:"+ (returnDate.isPresent() ? returnDate.get() : "") + ", adults:"+adults+", nonStop:"+nonStop);
+				+ "departureDate:"+departureDate+", returnDate:"+ (returnDate.isPresent() ? returnDate.get() : "") + ", adults:"+adults+", nonStop:"+nonStop + headersLog);
 		LocalDate dep = LocalDate.parse(departureDate);
 		Optional<LocalDate> ret =  (returnDate.isEmpty()) ? Optional.empty() : Optional.of(LocalDate.parse(returnDate.get()));
 		FlightOffersResponse resp = null;
@@ -76,20 +82,20 @@ public class MainController {
 		resp = fOService.getFlightOffers(originLocationCode, destinationLocationCode, dep, ret, adults, nonStop);
 		}
 		catch (Exception ex) {
-			logger.info("Exception Occured on Endpoint " + EndpointUrls.GET_OFFERS +" , Exception:"+ex);
-			logger.info("Returning Status code, "+ HttpStatus.INTERNAL_SERVER_ERROR);
+			logger.info("Exception Occured on Endpoint " + EndpointUrls.GET_OFFERS +" , Exception:"+ex + headersLog);
+			logger.info("Returning Status code, "+ HttpStatus.INTERNAL_SERVER_ERROR + headersLog);
 			return new ResponseEntity<>(new FlightOffersResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
 		if(resp== null || resp.getData()== null || resp.getData().size()==0) {
 			return new ResponseEntity<>(new FlightOffersResponse(), HttpStatus.NOT_FOUND);
 		}
-		logger.info("Returning Response, status: "+ HttpStatus.OK);
+		logger.info("Returning Response, status: "+ HttpStatus.OK + headersLog);
 		if(logger.isDebugEnabled()) {
 			try {
-				logger.debug("Resposne Returned : " + mapper.writeValueAsString(resp));
+				logger.debug("Resposne Returned : " + mapper.writeValueAsString(resp) + headersLog);
 			} catch (JsonProcessingException e) {
-				logger.info("Exception while writing value as String: " + e);
+				logger.info("Exception while writing value as String: " + e+ headersLog);
 			}
 		}
 		return new ResponseEntity<>(resp, HttpStatus.OK);
